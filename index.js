9b59d3eb02733ec5b4ac1e022a076f5f3513229e
haruymth <108157117+haruymth@users.noreply.github.com>
2023-04-07 12:31:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const fs = require("fs");
+const http = require("https");
 
+http.createServer(function(req,res){
+  res.writeHead(200,{
+    "content-type":"text/html;charset=utf8"
+  });
+  res.end(fs.readFileSync("page.html","utf8"));
+})
